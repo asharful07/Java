a430991e24155e8296f777d297b4c4f5c8adbadf
asharful07 <asharfulkhan202@gmail.com>
2023-09-01 22:24:48 +0530

    Question_03

---
+++ src/loops/Question_03.java
@@ -0,0 +1,12 @@
+
+// Write a program to reverse the Table of given number
+
+package loops;
+public class Question_03 {
+    public static void main(String[] args) {
+        int num = 10;
+        for(int i=num;i>0;i--){
+            System.out.println(num+" x "+i+" = "+(num*i));
+        }
+    }
+}
