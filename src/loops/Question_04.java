0416d0e61b5a12b4e0f62e23c168013660a793d8
asharful07 <asharfulkhan202@gmail.com>
2023-09-01 22:26:28 +0530

    Question_04

---
+++ src/loops/Question_04.java
@@ -0,0 +1,11 @@
+
+// Write a program to print all alphabets from A To Z
+
+package loops;
+public class Question_04 {
+    public static void main(String[] args) {
+        for(int i=65;i<=90;i++){
+            System.out.printf("%c ",i);
+        }
+    }
+}
