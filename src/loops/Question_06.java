5d5a12c38d7b4c807bf3bcb348fff444b60e198c
asharful07 <asharfulkhan202@gmail.com>
2023-09-03 12:57:13 +0530

    Question_06

---
+++ src/loops/Question_06.java
@@ -0,0 +1,11 @@
+
+// Write a program to print the alphabet from Z to A
+
+package loops;
+public class Question_06 {
+    public static void main(String[] args) {
+        for(int i=90;i>64;i--){
+            System.out.printf("%c ",i);
+        }
+    }
+}
