b9e9977cb0cde281fa7d906f761763e1d5345393
asharful07 <asharfulkhan202@gmail.com>
2023-09-17 22:29:32 +0530

    Question_18

---
+++ src/loops/Question_18.java
@@ -0,0 +1,13 @@
+
+// Write a program to count Total character in a String
+
+package loops;
+import java.util.Scanner;
+public class Question_18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.next();
+        System.out.println(str.length());
+    }
+}
