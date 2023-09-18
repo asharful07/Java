59c6980457827f8bc861f4a00868a6a1ffe1015f
asharful07 <asharfulkhan202@gmail.com>
2023-09-18 22:18:28 +0530

    Question_19

---
+++ src/loops/Question_19.java
@@ -0,0 +1,13 @@
+
+//Write a program To convert the String in Uppercase
+
+package loops;
+import java.util.Scanner;
+public class Question_19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.next();
+        System.out.println(str.toUpperCase());
+    }
+}
