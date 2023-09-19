5a0cd11a955e3006b1726441f0f007cba4cb12f8
asharful07 <asharfulkhan202@gmail.com>
2023-09-19 22:25:11 +0530

    Question_20

---
+++ src/loops/Question_20.java
@@ -0,0 +1,14 @@
+
+
+// Write a program To convert a String in Lowercase
+
+package loops;
+import java.util.Scanner;
+public class Question_20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.next();
+        System.out.println(str.toLowerCase());
+    }
+}
