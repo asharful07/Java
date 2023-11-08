1386666c2fa06155c9952002a77ff29bbe026f68
asharful07 <asharfulkhan202@gmail.com>
2023-11-08 22:29:24 +0530

    Question_15

---
+++ src/method/Question_15.java
@@ -0,0 +1,16 @@
+
+
+// Working of Method
+
+package method;
+public class Question_15 {
+        static void myMethod() {
+            System.out.println("I just got executed!");
+        }
+
+        public static void main(String[] args) {
+            myMethod();
+            myMethod();
+            myMethod();
+        }
+    }
