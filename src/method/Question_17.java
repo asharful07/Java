8e5b4b67cfce6727176373cc00e88b57a3c37a4a
asharful07 <asharfulkhan202@gmail.com>
2023-11-08 22:39:32 +0530

    Question_17

---
+++ src/method/Question_17.java
@@ -0,0 +1,16 @@
+
+
+// static method
+
+package method;
+public class Question_17 {
+        public static void main(String[] args)
+        {
+            show();
+        }
+        static void show()
+        {
+            System.out.println("It is an example of static method.");
+        }
+    }
+
